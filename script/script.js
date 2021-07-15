99c98e91d0bd8a1cfbb6abc5cdfe278ba9d3d9e7
Kenneth Walker <kennethesteban@hotmail.com>
2021-07-14 21:06:26 -0600

    Updates on JS

--- script/script.js
+++ script/script.js
@@ -1,1 +1,6 @@
-console.log("Testing");
+
+function ageInDays() {
+    var birthyear = prompt("Please type your Birth year");
+    var ageOnDays = (2021 - birthyear) * 365;
+    document.getElementById("answer").innerHTML = ("Result = " + ageOnDays);
+}
